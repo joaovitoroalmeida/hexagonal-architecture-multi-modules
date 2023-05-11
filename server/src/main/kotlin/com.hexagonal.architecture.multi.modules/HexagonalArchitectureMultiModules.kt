55d2283c2fe59502701cdb2cc856320717d554a6
João Vitor Almeida <jv.oa202@gmail.com>
2023-05-11 19:18:06 -0300

    feat: remove unused import in main class

--- server/src/main/kotlin/com.hexagonal.architecture.multi.modules/HexagonalArchitectureMultiModules.kt
+++ server/src/main/kotlin/com.hexagonal.architecture.multi.modules/HexagonalArchitectureMultiModules.kt
@@ -2,7 +2,6 @@ package com.hexagonal.architecture.multi.modules
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class HexagonalArchitectureMultiModules
